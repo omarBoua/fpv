d07ebb32773f60088eee86ff93dd824847a71ff3
Dhia Bouassida <dhia.bouassida@titnaom.com>
2021-11-07 20:24:00 +0100

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,29 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+//        for (int i = 4; i < 9999; i++) {
+//            if (i % 100 == 0) System.out.println(i);
+//
+//            for (int j = i-1500; j < i + 1500; j++) {
+//                Gleitpunktzahl a = new Gleitpunktzahl(i).add(new Gleitpunktzahl(j));
+//                Gleitpunktzahl b = new Gleitpunktzahl(i + j);
+//                if (a.mantisse != b.mantisse || a.exponent != b.exponent) {
+//                    System.out.println(new Gleitpunktzahl(i).add(new Gleitpunktzahl(j)));
+//                    System.out.println(new Gleitpunktzahl(i + j));
+//                }
+//            }
+//        }
+//        Gleitpunktzahl a = new Gleitpunktzahl(2045);
+//        Gleitpunktzahl c = new Gleitpunktzahl(0);
+//        Gleitpunktzahl b = new Gleitpunktzahl(2045);
+//        System.out.println(b.sub(c));
+//        System.out.println(a);
+//        System.out.println(b.sub(c).compareAbsTo(a));
+
+        double d = 0.0/0.0;
+        System.out.println(d+1.0/0);
+
+    }
+}
---
+++ src/com/company/hhh.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class hhh {
+    public static void main(String[] args) {
+        System.out.println("########");
+        System.out.println(1d/0 - 1d/0);
+    }
+}
